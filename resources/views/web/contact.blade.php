272a40ec25d604242ccd041b17db08ce887b5a55
Tony Starks <35855530+slik911@users.noreply.github.com>
2025-04-25 08:43:41 -0600

    Update contact.blade.php

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -60,7 +60,7 @@
           </form>
                 </div>
                 <div class="col-md-6 pt-3" style="border-radius: 10px">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.4955040005166!2d-112.8406813!3d49.8519362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x536e79706b47ac59%3A0x8d9b8cefd5ecbbfa!2s314%20Second%20St%2C%20Shaughnessy%2C%20AB%20T0K%202A0!5e0!3m2!1sen!2sca!4v1745581922493!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.4955040005166!2d-112.8406813!3d49.8519362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x536e79706b47ac59%3A0x8d9b8cefd5ecbbfa!2s314%20Second%20St%2C%20Shaughnessy%2C%20AB%20T0K%202A0!5e0!3m2!1sen!2sca!4v1745581922493!5m2!1sen!2sca" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
